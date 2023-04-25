b97e69e74b52efc071ad9a11e525b8f461be7eea
Ioannis Mertzanis <imertz@protonmail.com>
2023-04-25 15:58:13 +0300

    Added multiplication

--- index.ts
+++ index.ts
@@ -2,4 +2,6 @@ export const add = (a: number, b: number) => a + b;
 
 export const minus = (a: number, b: number) => a - b;
 
+export const multiply = (a: number, b: number) => a * b;
+
 // Testing my package
