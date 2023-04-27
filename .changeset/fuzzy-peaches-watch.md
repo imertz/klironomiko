9fcf9c2d3a68b9775c3ad57bc643605781770790
Ioannis Mertzanis <imertz@protonmail.com>
2023-04-27 12:50:44 +0300

    Changed crypto to webcrypto

---
+++ .changeset/fuzzy-peaches-watch.md
@@ -0,0 +1,5 @@
+---
+"klironomiko": patch
+---
+
+Changed crypto to webcrypto
--- index.ts
+++ index.ts
@@ -1,34 +1,53 @@
-import { randomUUID as uuid } from "crypto";
+import { webcrypto } from "crypto";
 
 type Relatives = {
-  spouse: boolean;
-  parents: number;
+  spouse: Spouse;
+  parents?: number;
   descendants: Descendant[];
+  siblings?: Descendant[];
   uuid?: string;
 };
 
 type Descendant = {
   fname: string;
-  relation: string;
+  relation: Relation;
   descendants?: Descendant[];
   share?: number;
   uuid?: string;
 };
 
+type Relation =
+  | "child"
+  | "grand-child"
+  | "great-grand-child"
+  | "sibling"
+  | "nephew"
+  | "grand-nephew";
+
 type RelativesWithUuid = {
-  spouse: boolean;
-  parents: number;
+  spouse: Spouse;
+  parents?: number;
   descendants: DescendantWithUuid[];
   uuid: string;
   share?: number;
+  fraction?: string;
 };
 type DescendantWithUuid = {
   fname: string;
-  relation: string;
+  relation: Relation;
   descendants?: DescendantWithUuid[];
   share?: number;
+  fraction?: string;
   uuid: string;
 };
+
+type Spouse = {
+  alive: boolean;
+  apodochi: boolean;
+  share?: number;
+  fraction?: string;
+};
+
 export function calculateHeirPercentage(relatives: Relatives) {
   // Add a random uuid without dashes to each leaf node of the relatives object
   // This is to ensure that each node is unique
@@ -36,7 +55,12 @@ export function calculateHeirPercentage(relatives: Relatives) {
     JSON.stringify(relatives)
   ); // Deep copy
   const addUuid = (relative: Relatives | Descendant) => {
-    relative.uuid = uuid().replace(/-/g, "");
+    // If uuid is already present, return
+    if (relative.uuid) {
+      return;
+    }
+
+    relative.uuid = webcrypto.randomUUID().replace(/-/g, "");
 
     if (relative.descendants) {
       relative.descendants.forEach((descendant) => {
@@ -47,95 +71,192 @@ export function calculateHeirPercentage(relatives: Relatives) {
   addUuid(relativesWithUuid);
 
   const percentages: { [key: string]: number } = {};
-  if (relativesWithUuid.spouse) {
-    percentages["spouse"] = 0.25;
+  if (relativesWithUuid.spouse.alive && relativesWithUuid.spouse.apodochi) {
+    relativesWithUuid.spouse = {
+      ...relativesWithUuid.spouse,
+      share: 0.25,
+      fraction: "1/4",
+    };
   }
-  const descendantPercentage = relativesWithUuid.spouse ? 0.75 : 1;
+  const descendantPercentage =
+    relativesWithUuid.spouse.alive && relativesWithUuid.spouse.apodochi
+      ? 0.75
+      : 1;
+
   const children: DescendantWithUuid[] = relativesWithUuid.descendants.filter(
     (relative: DescendantWithUuid) => relative.relation === "child"
   );
   const childrenCount = children.length;
-  const childrenPercentage =
-    childrenCount > 0 ? descendantPercentage / childrenCount : 0;
-  children.forEach((child) => {
-    percentages[child.uuid] = childrenPercentage;
-    const grandChildren = child.descendants?.filter(
-      (relative) => relative.relation === "grand-child"
-    );
-    const grandChildrenCount = grandChildren?.length ? grandChildren.length : 0;
-    const grandChildrenPercentage =
-      grandChildrenCount > 0 ? childrenPercentage / grandChildrenCount : 0;
+  if (childrenCount > 0) {
+    const childrenPercentage =
+      childrenCount > 0 ? descendantPercentage / childrenCount : 0;
+    children.forEach((child) => {
+      percentages[child.uuid] = childrenPercentage;
+      const grandChildren = child.descendants?.filter(
+        (relative) => relative.relation === "grand-child"
+      );
+      const grandChildrenCount = grandChildren?.length
+        ? grandChildren.length
+        : 0;
+      const grandChildrenPercentage =
+        grandChildrenCount > 0 ? childrenPercentage / grandChildrenCount : 0;
 
-    grandChildren &&
-      grandChildren?.forEach((grandChild) => {
-        percentages[grandChild.uuid] = grandChildrenPercentage;
-        const greatGrandChildren = grandChild?.descendants?.filter(
-          (relative) => relative.relation === "great-grand-child"
-        );
-        const greatGrandChildrenCount = greatGrandChildren?.length
-          ? greatGrandChildren.length
-          : 0;
-        const greatGrandChildrenPercentage =
-          greatGrandChildrenCount > 0
-            ? grandChildrenPercentage / greatGrandChildrenCount
+      grandChildren &&
+        grandChildren?.forEach((grandChild) => {
+          percentages[grandChild.uuid] = grandChildrenPercentage;
+          const greatGrandChildren = grandChild?.descendants?.filter(
+            (relative) => relative.relation === "great-grand-child"
+          );
+          const greatGrandChildrenCount = greatGrandChildren?.length
+            ? greatGrandChildren.length
             : 0;
-        greatGrandChildren?.forEach((greatGrandChild) => {
-          percentages[greatGrandChild.uuid] = greatGrandChildrenPercentage;
+          const greatGrandChildrenPercentage =
+            greatGrandChildrenCount > 0
+              ? grandChildrenPercentage / greatGrandChildrenCount
+              : 0;
+          greatGrandChildren?.forEach((greatGrandChild) => {
+            percentages[greatGrandChild.uuid] = greatGrandChildrenPercentage;
+          });
         });
-      });
-  });
-  // If there are no children, the entire estate goes to the spouse
-  if (childrenCount === 0 && relativesWithUuid.parents === 0) {
-    percentages["spouse"] = 1;
-  }
-  // If there are no children and no spouse, the entire estate goes to the parents
-  if (childrenCount === 0 && !relativesWithUuid.spouse) {
-    percentages["parents"] = 1;
-  }
-  // If a relative has a descendant, remove the relative from the percentages object
-  Object.keys(percentages).forEach((key) => {
-    if (
-      relativesWithUuid.descendants.find(
-        (relative) =>
-          relative.uuid === key &&
-          relative.descendants?.length &&
-          relative.descendants.length > 0
-      )
-    ) {
-      delete percentages[key];
+    });
+    // If there are no children, the entire estate goes to the spouse
+    if (childrenCount === 0 && relativesWithUuid.parents === 0) {
+      relativesWithUuid.spouse = {
+        ...relativesWithUuid.spouse,
+        share: 1,
+        fraction: "1/1",
+      };
     }
-  });
-  // If a descendant has a descendant, remove the descendant from the percentages object
-  Object.keys(percentages).forEach((key) => {
+    // If there are no children and no spouse, the entire estate goes to the parents
     if (
-      relativesWithUuid.descendants.find((relative) =>
-        relative.descendants?.find(
-          (descendant) =>
-            descendant.uuid === key &&
-            descendant.descendants?.length &&
-            descendant.descendants.length > 0
+      childrenCount === 0 &&
+      (relativesWithUuid.spouse.alive || !relativesWithUuid.spouse.apodochi)
+    ) {
+      percentages["parents"] = 1;
+    }
+    // If a relative has a descendant, remove the relative from the percentages object
+    Object.keys(percentages).forEach((key) => {
+      if (
+        relativesWithUuid.descendants.find(
+          (relative) =>
+            relative.uuid === key &&
+            relative.descendants?.length &&
+            relative.descendants.length > 0
         )
-      )
-    ) {
-      delete percentages[key];
-    }
-  });
+      ) {
+        delete percentages[key];
+      }
+    });
+    // If a descendant has a descendant, remove the descendant from the percentages object
+    Object.keys(percentages).forEach((key) => {
+      if (
+        relativesWithUuid.descendants.find((relative) =>
+          relative.descendants?.find(
+            (descendant) =>
+              descendant.uuid === key &&
+              descendant.descendants?.length &&
+              descendant.descendants.length > 0
+          )
+        )
+      ) {
+        delete percentages[key];
+      }
+    });
 
-  // Add the share of the estate to each relative in the relatives object
-  const addShare = (relative: RelativesWithUuid | DescendantWithUuid) => {
-    if (percentages[relative.uuid]) {
-      relative.share = percentages[relative.uuid];
-    }
-    if (relative.descendants) {
-      relative.descendants.forEach((descendant) => {
-        addShare(descendant);
-      });
-    }
-  };
-  addShare(relativesWithUuid);
+    // Add the share of the estate to each relative in the relatives object
+    const addShare = (relative: RelativesWithUuid | DescendantWithUuid) => {
+      if (percentages[relative.uuid]) {
+        relative.share = percentages[relative.uuid];
+        if (relative.share) {
+          relative.fraction = toFraction(relative.share);
+        }
+      }
+      if (relative.descendants) {
+        relative.descendants.forEach((descendant) => {
+          addShare(descendant);
+        });
+      }
+    };
+    addShare(relativesWithUuid);
 
-  // Return the relatives object
-  return relativesWithUuid;
+    // Return the relatives object
+    return relativesWithUuid;
+  }
+
+  if (childrenCount === 0 && relativesWithUuid.parents === 0) {
+    relativesWithUuid.spouse = {
+      ...relativesWithUuid.spouse,
+      share: 1,
+      fraction: "1/1",
+    };
+    return relativesWithUuid;
+  }
+  if (
+    childrenCount === 0 &&
+    (relativesWithUuid.spouse.alive || !relativesWithUuid.spouse.apodochi)
+  ) {
+    percentages["parents"] = 1;
+    return relativesWithUuid;
+  }
+  if (
+    childrenCount === 0 &&
+    relativesWithUuid.spouse.alive &&
+    relativesWithUuid.spouse.apodochi &&
+    relativesWithUuid.parents === 0
+  ) {
+    relativesWithUuid.spouse = {
+      ...relativesWithUuid.spouse,
+      share: 1,
+      fraction: "1/1",
+    };
+    return relativesWithUuid;
+  }
 }
 
 // Testing my package
+const heirPercentage = calculateHeirPercentage({
+  spouse: {
+    alive: true,
+    apodochi: true,
+  },
+  parents: 2,
+  descendants: [
+    {
+      fname: "John",
+      relation: "child",
+      descendants: [
+        {
+          fname: "John's child",
+          relation: "grand-child",
+          descendants: [
+            {
+              fname: "John's grandchild",
+              relation: "great-grand-child",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+});
+
+// Pretty print the heir percentage
+console.log(JSON.stringify(heirPercentage, null, 2));
+
+function toFraction(decimal: number) {
+  const tolerance = 1.0e-10; // set a tolerance for floating-point comparison
+  let numerator = 1;
+  let denominator = 1;
+  let error = decimal - numerator / denominator;
+
+  while (Math.abs(error) > tolerance) {
+    if (error > 0) {
+      numerator++;
+    } else {
+      denominator++;
+    }
+    error = decimal - numerator / denominator;
+  }
+
+  return numerator + "/" + denominator;
+}
